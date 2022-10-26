ad2260786f3e03b36ef1c6fbbd20a1a9bf639034
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-26 10:28:07 +0200

    auto-commit-1281a254c55d4ef9889dfcc9dafb7447

--- guessing.cpp
+++ guessing.cpp
@@ -7,7 +7,7 @@ int main(){
     fstream file;
     string word;
     int category;
-    cout << "Category";
+    cout << "Category"<<endl;
     cout << "1. Animals"<<endl<< "2. Teams"<<endl << "3. Districts"<<endl<<"4. Films"<<endl<<"5. Books"<<endl;
     cat:
     cout << "Please select a category: ";
@@ -19,9 +19,9 @@ int main(){
     }else if (category == 3){
         file.open("category/districts.txt", ios::in);
     }else if(category == 4){
-        file.open("category/films.txt", ios::in);
+        file.open("./category/films.txt", ios::in);
     }else if(category == 5){
-        file.open("category/books.txt", ios::in);
+        file.open("./category/books.txt", ios::in);
     }else{
         cout << "Please select a valid number"<<endl;
         goto cat;
@@ -31,4 +31,5 @@ int main(){
         file >> word;
         cout << word << endl;
     }
+    return 0;
 }
