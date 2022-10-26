3943198f5d1ef5133161ef47f8e4cf8597e1393e
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-26 14:58:20 +0200

    Finished setting up the C++ guessing game

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# guessing_game
+# Guessing game
+
+This is a game built in form of a c++ program to enhance and sharpen people's minds.
+It was built for learning purposes but eventually became a way of improving our knowledge.
+
+# Features
+The game has five categories in .txt files:
+
+- Animals(General)
+- Teams(Football)
+- Districts(Rwanda)
+- Films(2010)
+- Books(One word)
+  
+# Each category has 50 lines expect for districts with 30 lines
+
+# Steps
+
+1.Select the category
+2.Guess the word by inputing the desirable letter
+3.Terminate using 0 or Ctr+c
+
+# Any errors were removed
--- guessingGame.cpp
+++ guessingGame.cpp
@@ -13,55 +13,63 @@ int main(){
     bool keepPlaying = true;
     while(keepPlaying){
 
-    cout <<endl<< "GUESSING GAME"<<endl<<endl;
+    cout <<endl<< "\t\t\tGUESSING GAME"<<endl<<endl;
         int category;
-        cout << "Category" << endl;
+        cout << "\t\tCategory" << endl;
         cout << "1. Animals" << endl
              << "2. Teams" << endl
              << "3. Districts" << endl
              << "4. Films" << endl
              << "5. Books" << endl;
     cat:
-        cout << "Please select a category: ";
+        cout <<endl<< "Please select a category: ";
         cin >> category;
-        if (category == 1)
+
+        switch (category)
         {
+        case 1:
             file.open("category/animals.txt", ios::in);
-        }
-        else if (category == 2)
-        {
+            break;
+        case 2:
             file.open("category/teams.txt", ios::in);
-        }
-        else if (category == 3)
-        {
+            break;
+        case 3:
             file.open("category/districts.txt", ios::in);
-        }
-        else if (category == 4)
-        {
-            file.open("./category/films.txt", ios::in);
-        }
-        else if (category == 5)
-        {
-            file.open("./category/books.txt", ios::in);
-        }
-        else
-        {
-            cout << "Please select a valid number" << endl;
+            break;
+        case 4:
+            file.open("category/films.txt",ios::in);
+            break;
+        case 5:
+            file.open("category/books.txt", ios::in);
+            break;
+        default:
+            if (category == 0)
+            {   
+                end:
+                cout << endl
+                     << "Terminating program..." << endl;
+                return 0;
+            }
+            cout << "Please select a valid number!!" << endl;
             goto cat;
-            if(category == 0){
-                cout << "Terminating program..."<<endl;
-            }
-            if(!file){
-                cout << "File not found!" << endl;
-            }
+            break;
+        }
+        if (!file)
+        {
+            cout << endl<<"Category not found!" << endl;
+            goto end;
         }
         int choice;
         bool valid = false;
 
         while (!valid)
-        {
+        {   
+            int max = 50;
+            if(category == 3){
+                max = 30;
+            }
             srand(time(0));
-            choice = (rand() % 50) + 1;
+            choice = (rand() % max) + 1;
             valid = true;
         }
         string word;
@@ -82,20 +90,21 @@ int main(){
         {
             c = '_';
         }
-        int guesses = 7;
+        int guesses = word.length();  
         bool gameOver = false;
         while (!gameOver)
         {
             cout << "The word is " << blanks << endl;
             if (guesses == 1)
             {
-                cout << "You have " << guesses << " guess left." << endl;
+
+                cout <<endl<< "Watch out!"<<endl<<"You have " << guesses << " guess left." << endl;
             }
             else
             {
-                cout << "You have " << guesses << " guesses left." << endl;
+                cout <<endl<< "You have " << guesses << " guesses left." << endl;
             }
-            cout << "Please enter a letter to guess: ";
+            cout << endl<< "Please enter a letter to guess: ";
             char letter;
             cin >> letter;
 
@@ -112,28 +121,36 @@ int main(){
             }
             if (!match)
             {
-                cout << "Sorry, the word does not contain the letter '" << letter << "'" << endl;
+                cout <<endl<< "Sorry, the word does not contain the letter '" << letter << "'" << endl;
                 guesses--;
             }
             if (blanks == word)
             {
                 gameOver = true;
-                cout << "Congratulations! You are such a great game player" << endl;
-                cout << "The word is " << word << endl;
+                cout << endl<<"Congratulations! You are such a great game player" << endl;
+                cout <<endl<< "The word is '" << word <<"'"<< endl;
             }
             if (guesses == 0)
             {
                 gameOver = true;
-                cout << "You are out of guesses. I am sure you will win next time" << endl;
-                cout << "The word is " << word << endl;
+                cout << endl<<"You have run out of guesses. I am sure you will win next time" << endl;
+                cout <<endl<<"The word is '" << word <<"'"<< endl;
+            }
+            if(gameOver == true){
+                file.close();
             }
         }
-        cout << "Would you like to continue playing? (y/n): ";
+        cout << endl<<"Would you like to continue playing? (y/n): ";
         char playagain;
         cin >> playagain;
         if(playagain == 'n'){
             keepPlaying = false;
-            cout << "Farewell , kind spirit." << endl<< endl<<"Thank you for playing." << endl;
+            cout << endl<<"Farewell , kind spirit." << endl<< endl<<"\t\tThank you for playing." << endl;
+        }else if(playagain == 'y'){
+            keepPlaying = true;
+        }else{
+            cout << endl<< "Please enter a valid input(y/n): " << endl;
+            cin >> playagain;
         }
     }
 }
